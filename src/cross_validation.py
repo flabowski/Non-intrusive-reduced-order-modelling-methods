4f114a1ec7454a212a1f9c1f524c5995dc7df08d
Florian Maurits Arbes <florian.arbes@ife.no>
2021-01-29 14:40:10 +0100

    cleaned up and added nice plots

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -6,7 +6,6 @@ Created on Fri Jan 22 13:42:51 2021
 """
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import os
 import pickle
@@ -14,18 +13,37 @@ import tensorflow as tf
 import timeit
 from scipy.optimize import curve_fit
 from scipy.interpolate import Rbf, griddata
+import seaborn as sns
+import pandas as pd
 # from mayavi import mlab
 np.set_printoptions(suppress=True)
 plot_width = 16
 
 
-def snapshots(file, snapshots_per_dataset):
+def select_random_snapsots(U_u, U_v, U_p, t, snapshots_per_dataset):
+    n_nodes, n_datasets, snapshots_per_dataset_all = U_u.shape
+    U_u_resized = np.empty((n_nodes, n_datasets, snapshots_per_dataset))
+    U_v_resized = np.empty((n_nodes, n_datasets, snapshots_per_dataset))
+    U_p_resized = np.empty((n_nodes, n_datasets, snapshots_per_dataset))
+    t_resized = np.empty((n_datasets, snapshots_per_dataset))
+    for i in range(n_datasets):
+        N = snapshots_per_dataset_all
+        inds = np.sort(np.random.randint(low=0, high=N,
+                                         size=snapshots_per_dataset))
+        U_u_resized[:, i, :] = U_u[:, i, inds]
+        U_v_resized[:, i, :] = U_v[:, i, inds]
+        U_p_resized[:, i, :] = U_p[:, i, inds]
+        t_resized[i, :] = t[i, inds]
+    return U_u_resized, U_v_resized, U_p_resized, t_resized
+
+
+def xxxxx(file, snapshots_per_dataset):
     data = pickle.load(open(file, "rb"))
     N = len(data["time"])
     inds = np.sort(np.random.randint(0, N, size=snapshots_per_dataset))
     u, v, p = data["u"][:, inds], data["v"][:, inds], data["p"][:, inds]
     t = data["time"][inds]-data["time"][0]
-    print(file, data["Re"], data["rho"], data["mu"], t[[0, -1]])
+    print(file, len(data["t"]), len(t), data["Re"], data["rho"], data["mu"])
     return u, v, p, t, data["mu"]
 
 
@@ -82,7 +100,6 @@ def load_snapshots(path, snapshots_per_dataset):
 
     n_nodes = len(data["x"])
     n_datasets = len(files)
-    n_snapshots = snapshots_per_dataset*n_datasets
 
     U_u = np.zeros((n_nodes, n_datasets, snapshots_per_dataset))
     U_v = np.zeros((n_nodes, n_datasets, snapshots_per_dataset))
@@ -90,9 +107,16 @@ def load_snapshots(path, snapshots_per_dataset):
     t = np.zeros((n_datasets, snapshots_per_dataset))
     mu = np.zeros((n_datasets))
     for i, file in enumerate(files):
-        # s, e = i*snapshots_per_dataset, (i+1)*snapshots_per_dataset
-        U_u[:, i], U_v[:, i], U_p[:, i], t[i], mu[i] = \
-            snapshots(path+file, snapshots_per_dataset)
+        data = pickle.load(open(path+file, "rb"))
+        N = len(data["time"])
+        inds = np.sort(np.random.randint(0, N, size=snapshots_per_dataset))
+        U_u[:, i, :] = data["u"][:, inds]
+        U_v[:, i, :] = data["v"][:, inds]
+        U_p[:, i, :] = data["p"][:, inds]
+        t[i] = data["time"][inds]-data["time"][0]
+        mu[i] = data["mu"]
+        print(file, ":\n", len(data["time"]), len(t[i]),
+              data["Re"], data["rho"], data["mu"])
     return U_u, U_v, U_p, t, mu, data["x"], data["y"], data["tri"]
 
 
@@ -194,6 +218,7 @@ def normalize_phase(V, t, mu):
     time = t.reshape(n_datasets, snapshots_per_dataset)
     period = np.empty((n_datasets, ))
     amplitude = np.empty((n_datasets, ))
+    phase = np.empty((n_datasets, ))
     time_corrected = np.zeros_like(time)
     fits = n_datasets*[None]
 
@@ -208,6 +233,7 @@ def normalize_phase(V, t, mu):
         # fit[0][2] = 0.
         period[i] = fit[0][0]
         amplitude[i] = fit[0][1]
+        phase[i] = fit[0][2]
         time_corrected[i] = time[i]-fit[0][2]  # np.mod(time[i]-fit[0][2], period[i])
     Re = .1 / mu
 
@@ -256,7 +282,7 @@ def normalize_phase(V, t, mu):
     ax1.set_title("period")
     ax2.set_title("amplitude")
     plt.show()
-    return time_corrected, period
+    return phase, period
 
 
 if __name__ == "__main__":
@@ -265,15 +291,187 @@ if __name__ == "__main__":
     # n: number of nodes
     # m: number of snapshots = n_snapshots = n_datasets * snapshots_per_dataset
     # r: truncation rank
+    # U_x snapshot matrix. SVD: U = U*S*V
 
-    snapshots_per_dataset = 50  # random choice!
-    n_modes = 10
+    n_modes = 15
     path = "C:/Users/florianma/Documents/data/flow_around_cylinder/"
 
     plt.close("all")
-    U_u, U_v, U_p, t, mu, x, y, tri = load_snapshots(path, snapshots_per_dataset)
+    U_u_all, U_v_all, U_p_all, _t_all_, mu, x, y, tri = load_snapshots(path, 800)
+    U_u, U_v, U_p, _t_ = select_random_snapsots(U_u_all, U_v_all, U_p_all,
+                                                _t_all_, 150)
     S_full, U_full, V_full, M_full = create_ROM(U_u, U_v, U_p)
+    phase, period = normalize_phase(V_full, _t_, mu)
+    t_all = _t_all_ - phase[:, None]
+    t = _t_ - phase[:, None]
 
+    u_m = np.mean(np.mean(U_u, axis=1), axis=1)
+    v_m = np.mean(np.mean(U_v, axis=1), axis=1)
+    p_m = np.mean(np.mean(U_p, axis=1), axis=1)
+    fig, ax = plot_up(u_m, v_m, p_m, x, y, tri)
+    plt.show()
+    plot_eigenfaces(U_full, x, y, tri)
+    plot_mode_amplitude(S_full, V_full, t, mu)
+
+    n_nodes, n_datasets, n_snapshots_per_dataset = U_u.shape
+    trainingset = np.empty((n_datasets,), dtype=np.bool)
+    snapshots_per_dataset = 50  # random choice!
+    n_ss = [3, 5, 10, 20, 50, 100, 200]
+    mse = np.zeros((3, len(n_ss), n_datasets))
+    x_bp = np.zeros((3, len(n_ss), n_datasets), dtype=np.int32)
+    set_nr = np.zeros((3, len(n_ss), n_datasets), dtype=np.int32)
+    for ns, snapshots_per_dataset in enumerate(n_ss):
+        U_u, U_v, U_p, t = select_random_snapsots(U_u_all, U_v_all, U_p_all,
+                                                  t_all, snapshots_per_dataset)
+        snapshots = [U_u, U_v, U_p]
+        n_testset = (n_datasets-1)*snapshots_per_dataset
+        x1 = mu[:, None] * np.ones((n_datasets, 3*snapshots_per_dataset))  # 9, 3*150
+        x2 = np.c_[t-period[:, None], t, t+period[:, None]]  # 9, 3*150
+        for i in range(1, n_datasets-1):
+            s, e = i, (i+1)
+            trainingset[:] = True
+            trainingset[s:e] = False
+            validationset = ~trainingset
+            # need to copy to make array contiguous
+            S, U, V, M = create_ROM(U_u[:, trainingset].copy(),
+                                    U_v[:, trainingset].copy(),
+                                    U_p[:, trainingset].copy())
+            x1_train = x1[trainingset, :]
+            x2_train = x2[trainingset, :]  # 8, 3*150
+            interpolators = 3 * [n_modes * [None]]
+            x1_validation = mu[i] * np.ones((snapshots_per_dataset))
+            x2_validation = t[i, :]
+            # V_interpolated = 3*[np.zeros((snapshots_per_dataset, n_modes))]
+            V_interpolated = [np.zeros((snapshots_per_dataset, n_modes)),
+                              np.zeros((snapshots_per_dataset, n_modes)),
+                              np.zeros((snapshots_per_dataset, n_modes))]
+            n_trainingsets = trainingset.sum()
+            res = 3*[np.zeros((n_nodes, snapshots_per_dataset))]
+            for j in range(3):
+                for k in range(n_modes):
+                    d_ = V[j].numpy()[:, k].reshape(n_trainingsets, snapshots_per_dataset).copy()
+                    d = np.c_[d_, d_, d_]  # 8, 3*150 repeats in each oscillation
+                    # radial basis function interpolator instance
+                    # rbfi = Rbf(x1_train.ravel(), x2_train.ravel(), d.ravel())
+                    # interpolators[j][k] = rbfi
+                    points = np.c_[x1_train.ravel(), x2_train.ravel()]
+                    di = griddata(points, d.ravel(),
+                                  (x1_validation, x2_validation), method='linear')
+                    # rbfi = interpolators[j][k]
+                    # di = rbfi(x1_validation, x2_validation)
+                    V_interpolated[j][:, k] = di.copy()
+                U_hat = U[j].numpy()[:, :n_modes]  # (n, r)
+                S_hat = S[j].numpy()[:n_modes]  # (r, r) / (r,) wo  non zero elements
+                V_hat = V_interpolated[j]  # (r, m)
+                res[j] = np.dot(U_hat, S_hat[:, None]*V_hat.T) + M[j].numpy()  # (n, m)
+                mse[j, ns, i] = ((snapshots[j][:, i, :] - res[j])**2).mean()
+                x_bp[j, ns, i] = snapshots_per_dataset
+                set_nr[j, ns, i] = j
+                print(i, j, mse[j, ns, i])
+
+            if mu[i] == .002 and snapshots_per_dataset == 100:
+                u_int = res[0]
+                v_int = res[1]
+                p_int = res[2]
+                u_orig = U_u[:, i, :]
+                v_orig = U_v[:, i, :]
+                p_orig = U_p[:, i, :]
+                for ts in [0, 15]:
+                    fig, ax = plot_up(u_orig[:, ts], v_orig[:, ts],
+                                      p_orig[:, ts], x, y, tri)
+                    plt.suptitle("FOM")
+                    plt.show()
+
+                    fig, ax = plot_up(u_int[:, ts], v_int[:, ts], p_int[:, ts],
+                                      x, y, tri)
+                    plt.suptitle("ROM")
+                    plt.show()
+
+    df = pd.DataFrame(columns=['accuracy (mse)',
+                               '# snapshots in training set (per simulation)',
+                               'data set #'],
+                      data=np.c_[mse[:, :, 1:-1].ravel(),
+                                 x_bp[:, :, 1:-1].ravel(),
+                                 set_nr[:, :, 1:-1].ravel()])
+
+    fig, ax = plt.subplots(figsize=(plot_width/2.54, 10/2.54))
+    sns.boxplot(x='# snapshots in training set (per simulation)',
+                y='accuracy (mse)', hue='data set #', data=df)
+    sns.stripplot(x='# snapshots in training set (per simulation)',
+                  y='accuracy (mse)', hue='data set #', data=df,
+                  jitter=False, dodge=True, color='black', label='_nolegend')
+    # Fix the legend
+    handles, labels = ax.get_legend_handles_labels()
+    L = ax.legend(handles[0:3], labels[0:3])
+    L.get_texts()[0].set_text('velocity (x-component)')
+    L.get_texts()[1].set_text('velocity (y-component)')
+    L.get_texts()[2].set_text('pressure')
+    plt.suptitle("Accuracy of the ROM (cross-validation)")
+
+
+# old stuff
+    #         # V_hat1 = V[j].numpy()[:, :n_modes]  # (r, m)
+
+    #         U_red = U_full[j].numpy()[:, :n_modes]
+    #         S_red = S_full[j].numpy()[:n_modes]
+    #         V_red = V_full[j].numpy()[:, :n_modes]
+
+
+    #         print("V interpolated the same as in V?") # nope
+    #         for tmp_i in range(50):
+    #             print(np.allclose(V_interpolated[j][tmp_i, :], V[j].numpy()[250+tmp_i, :n_modes], atol=1e-6), end=", ")
+    #         print()
+
+    #         # print(V_hat.T[:, 0]*S_hat)
+    #         # print(V_hat2.T[:, 300]*S_hat)
+
+    #         res0[j] = np.dot(U_red, S_red[:, None]*V_red.T) + M_full[j].numpy()
+    #         res0[j].shape = n_nodes, n_datasets, n_snapshots_per_dataset
+
+    #         res0[j][:, i, :]-res[j]
+
+    #         print(np.allclose(U_red, U_hat))
+    #         print(np.allclose(S_red, S_hat))
+    #         # print(np.allclose(V_red, V_hat1))  # yes
+    #         # print(np.allclose(V_red[250:300], V_hat))  # no
+
+    #         # print(np.allclose(V_red.T[:, 250], V_hat.T[:, 0], atol=1e-6))  # no
+    #         # print(np.allclose(V_red.T[:, 265], V_hat.T[:, 15], atol=1e-6))  # no
+    #         # print(np.allclose(M[j].numpy(), M_full[j].numpy()))
+    #         # print()
+    #         print(np.allclose(res0[j][:, 5, :], res[j], atol=1e-6))  # no
+    #         print()
+    # # test: testset i_orig = 5
+    # for ts in [0, 15]:
+    #     i_orig = 5
+    #     res0[1].shape = n_nodes, n_datasets, n_snapshots_per_dataset
+    #     res0[2].shape = n_nodes, n_datasets, n_snapshots_per_dataset
+    #     for j in range(3):
+    #         print(np.allclose(res0[j][:, i_orig, :], res[j], atol=1e-6))
+    #         print(np.allclose(res0[j][:, i_orig, ts], res[j][:, ts], atol=1e-6))
+
+    #     u_int = res0[0][:, i_orig, ts]
+    #     v_int = res0[1][:, i_orig, ts]
+    #     p_int = res0[2][:, i_orig, ts]
+    #     u_int2 = res[0][:, ts]
+    #     v_int2 = res[1][:, ts]
+    #     p_int2 = res[2][:, ts]
+    #     np.allclose(u_int, u_int2)
+
+    #     fig, ax = plot_up(u_int, v_int, p_int, x, y, tri)
+    #     plt.title("ROM containing test data")
+    #     plt.show()
+
+
+    #     # plt.plot(S[i])
+
+
+
+    # # TODO: define t0 according to
+    # # get_data()
+    # # for root, dirs, files in os.walk("C:/Users/florianma/Documents/data/flow_around_cylinder/"):
+    # #     for filename in files:
+    #         print(filename)
 
     # tf_u_approx = tf.add(tf.matmul(U[0], tf.matmul(tf.linalg.diag(S[0]), V[0], adjoint_b=True)), mean_tensor[0]).numpy()
     # tf_v_approx = tf.add(tf.matmul(U[1], tf.matmul(tf.linalg.diag(S[1]), V[1], adjoint_b=True)), mean_tensor[1]).numpy()
@@ -292,144 +490,3 @@ if __name__ == "__main__":
     # plt.show()
     # fig, ax = plot_up(np_u_approx2[:, _t_], np_v_approx2[:, _t_], np_p_approx2[:, _t_], x, y, tri)
     # plt.show()
-
-
-
-
-
-
-    u_m = np.mean(np.mean(U_u, axis=1), axis=1)
-    v_m = np.mean(np.mean(U_v, axis=1), axis=1)
-    p_m = np.mean(np.mean(U_p, axis=1), axis=1)
-    fig, ax = plot_up(u_m, v_m, p_m, x, y, tri)
-    plt.show()
-    plot_eigenfaces(U_full, x, y, tri)
-    plot_mode_amplitude(S_full, V_full, t, mu)
-
-    time_corrected, period = normalize_phase(V_full, t, mu)
-
-    n_nodes, n_datasets, n_snapshots_per_dataset = U_u.shape
-    n_testset = (n_datasets-1)*snapshots_per_dataset
-    trainingset = np.empty((n_datasets,), dtype=np.bool)
-    # (n_nodes, n_datasets, snapshots_per_dataset)
-    x1 = mu[:, None] * np.ones((n_datasets, 3*snapshots_per_dataset))  # 9, 3*150
-    x2 = np.c_[time_corrected-period[:, None],
-               time_corrected,
-               time_corrected+period[:, None]]  # 9, 3*150
-    # for i in range(n_datasets):
-    if True:
-        i = 5
-        s, e = i, (i+1)
-        trainingset[:] = True
-        # trainingset[s:e] = False
-        validationset = ~trainingset
-        # need to copy to make array contiguous
-        S, U, V, M = create_ROM(U_u[:, trainingset].copy(),
-                                U_v[:, trainingset].copy(),
-                                U_p[:, trainingset].copy())
-        x1_train = x1[trainingset, :]
-        x2_train = x2[trainingset, :]  # 8, 3*150
-        interpolators = 3 * [n_modes * [None]]
-        x1_validation = mu[i] * np.ones((snapshots_per_dataset))
-        x2_validation = time_corrected[i, :]
-        # V_interpolated = 3*[np.zeros((snapshots_per_dataset, n_modes))]
-        V_interpolated = [np.zeros((snapshots_per_dataset, n_modes)),
-                          np.zeros((snapshots_per_dataset, n_modes)),
-                          np.zeros((snapshots_per_dataset, n_modes))]
-        for j in range(3):
-            for k in range(n_modes):
-                d_ = V[j].numpy()[:, k].reshape(n_datasets, snapshots_per_dataset).copy()
-                d = np.c_[d_, d_, d_]  # 8, 3*150 repeats in each oscillation
-                # radial basis function interpolator instance
-                # rbfi = Rbf(x1_train.ravel(), x2_train.ravel(), d.ravel())
-                # interpolators[j][k] = rbfi
-                points = np.c_[x1_train.ravel(), x2_train.ravel()]
-                di = griddata(points, d.ravel(),
-                              (x1_validation, x2_validation), method='linear')
-                # rbfi = interpolators[j][k]
-                # di = rbfi(x1_validation, x2_validation)
-                V_interpolated[j][:, k] = di.copy()
-        # V_interpolated[j][0, :]
-        # V[j].numpy()[300, :n_modes]
-            print("V interpolated the same as in V?") # yep
-            for tmp_i in range(50):
-                print(np.allclose(V_interpolated[j][tmp_i, :], V[j].numpy()[250+tmp_i, :n_modes], atol=1e-6))
-            print()
-
-        res0 = 3*[np.zeros((n_nodes, snapshots_per_dataset))]
-        res = 3*[np.zeros((n_nodes, snapshots_per_dataset))]
-        for j in range(3):
-            U_red = U_full[j].numpy()[:, :n_modes]
-            S_red = S_full[j].numpy()[:n_modes]
-            V_red = V_full[j].numpy()[:, :n_modes]
-
-            U_hat = U[j].numpy()[:, :n_modes]  # (n, r)
-            S_hat = S[j].numpy()[:n_modes]  # (r, r) / (r,) wo  non zero elements
-            V_hat1 = V[j].numpy()[:, :n_modes]  # (r, m)
-            V_hat = V_interpolated[j]  # (r, m)
-
-            print("V interpolated the same as in V?") # nope
-            for tmp_i in range(50):
-                print(np.allclose(V_interpolated[j][tmp_i, :], V[j].numpy()[250+tmp_i, :n_modes], atol=1e-6), end=", ")
-            print()
-
-            # print(V_hat.T[:, 0]*S_hat)
-            # print(V_hat2.T[:, 300]*S_hat)
-
-            res0[j] = np.dot(U_red, S_red[:, None]*V_red.T) + M_full[j].numpy()
-            res[j] = np.dot(U_hat, S_hat[:, None]*V_hat.T) + M[j].numpy()  # (n, m)
-            res0[j].shape = n_nodes, n_datasets, n_snapshots_per_dataset
-
-            print(np.allclose(U_red, U_hat))
-            print(np.allclose(S_red, S_hat))
-            print(np.allclose(V_red, V_hat1))  # yes
-            print(np.allclose(V_red[250:300], V_hat))  # no
-
-            print(np.allclose(V_red.T[:, 250], V_hat.T[:, 0], atol=1e-6))  # no
-            print(np.allclose(V_red.T[:, 265], V_hat.T[:, 15], atol=1e-6))  # no
-            print(np.allclose(M[j].numpy(), M_full[j].numpy()))
-            print()
-            print(np.allclose(res0[j][:, 5, :], res[j], atol=1e-6))  # no
-            print()
-    # test: testset i_orig = 5
-    for ts in [0, 15]:
-        i_orig = 5
-        res0[1].shape = n_nodes, n_datasets, n_snapshots_per_dataset
-        res0[2].shape = n_nodes, n_datasets, n_snapshots_per_dataset
-        for j in range(3):
-            print(np.allclose(res0[j][:, i_orig, :], res[j], atol=1e-6))
-            print(np.allclose(res0[j][:, i_orig, ts], res[j][:, ts], atol=1e-6))
-
-        u_int = res0[0][:, i_orig, ts]
-        v_int = res0[1][:, i_orig, ts]
-        p_int = res0[2][:, i_orig, ts]
-        u_int2 = res[0][:, ts]
-        v_int2 = res[1][:, ts]
-        p_int2 = res[2][:, ts]
-        np.allclose(u_int, u_int2)
-
-        fig, ax = plot_up(u_int, v_int, p_int, x, y, tri)
-        plt.title("ROM containing test data")
-        plt.show()
-
-
-        # u_orig = U_u[:, i_orig, ts]
-        # v_orig = U_v[:, i_orig, ts]
-        # p_orig = U_p[:, i_orig, ts]
-        # fig, ax = plot_up(u_orig, v_orig, p_orig, x, y, tri)
-        # plt.title("FOM")
-        # plt.show()
-
-        fig, ax = plot_up(u_int2, v_int2, p_int2, x, y, tri)
-        plt.title("ROM without test data")
-        plt.show()
-
-        # plt.plot(S[i])
-
-
-
-    # TODO: define t0 according to
-    # get_data()
-    # for root, dirs, files in os.walk("C:/Users/florianma/Documents/data/flow_around_cylinder/"):
-    #     for filename in files:
-    #         print(filename)
